0bc08937387eaeb53d4538b407f75ab3fb51820c
VOID_133 <zhangjianqiu_133@yeah.net>
2014-05-24 19:48:51 +0800

    Update and rename 4-002 first edi.cpp to 4-002.cpp
    更完整的版本 自定义String类 不过 还是不太理解运算符重载

--- C-Programming/4-002.cpp
+++ C-Programming/4-002.cpp
@@ -10,7 +10,21 @@ public:
 	~String();
 	const char* c_str() { return str; }
 	char *operator =(const char *s);
-	String(const char &p)
+	String & operator = (const String & s)
+	{
+		if (str == s.str) return *this;
+		if (str) delete[] str;
+		str = new char[strlen(str) + 1];
+		strcpy(str, s.str);
+		return *this;
+	}
+};
+
+class MyString {
+private:
+	String str;
+private:
+	String & operator =(const String & s);
 };
 
 char *String::operator =(const char *s)						//这是深拷贝 而默认的构造函数是浅拷贝  
@@ -35,6 +49,10 @@ int main(void)
 {
 	String s;
 	s = "Hello C++";
+	String s1;
+	s1 = "PPPP";
+	s = s1;					//这个代码在没有 String & operator = 的时候会出错
+	s = s;					//如果不在  String & operator =  里加入 语句A 第一个cout 会出异常值
 	cout << s.c_str() << endl;
 	s = "Good Luck";
 	cout << s.c_str() << endl;
