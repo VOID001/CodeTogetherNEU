f5e0dd629feb83bdaa7e42a5855deb70107f71bb
VOID_133 <zhangjianqiu_133@yeah.net>
2014-05-28 23:54:12 +0800

    Update UVA 550.cpp
    没改算法只做了优化 还是TLE

--- C-Programming/UVA 550.cpp
+++ C-Programming/UVA 550.cpp
@@ -6,35 +6,40 @@
 const int MAX = 10000;
 using namespace std;
 
-int arr[MAX+1];
+int arr[MAX + 1];
 int len = 0;					//数字位数
 bool no;
-int arrt[MAX+1];
+int arrt[MAX + 1];
 void arrMulti(int base, int fac)
 {
 	int addi = 0;
-	for (int i = MAX, j = 1; j <= len;j++,i--)
+	for (int i = MAX, j = 1; j <= len; j++, i--)
 	{
-		int tmp = arr[i] * fac+addi;
-		addi = tmp/base;
+		int tmp = arr[i] * fac + addi;
+		addi = tmp / base;
 		arr[i] = tmp%base;
-		if (j == len && addi > 0) no = 1;					//如果数组的位数都不一样 那么就不进行判断了
+		if (j == len && addi > 0) //如果数组的位数都不一样 那么就不进行判断了
+		{
+			no = 1;
+			for (int t = i; t <= MAX - 1; t++)
+				arrt[t] = base - 1;			//让这个数直接进位
+		}
 	}
 	return;
 }
-void arrPlus(int arrr[],int base)
+void arrPlus(int arrr[], int base)
 {
 	int addi = 0;
-	int i = MAX-1;
-	int tmp = arrr[i]+addi+1;
-	addi = tmp/base;
+	int i = MAX - 1;
+	int tmp = arrr[i] + addi + 1;
+	addi = tmp / base;
 	arrr[i] = tmp%base;
 	while (addi)
 	{
 		i--;
 		//tmp = arrr[i]+addi+1;
-		tmp = arrr[i]+addi;
-		addi = tmp/base;
+		tmp = arrr[i] + addi;
+		addi = tmp / base;
 		arrr[i] = tmp%base;
 		if (MAX - i + 1 > len) len++;
 	}
@@ -42,10 +47,10 @@ void arrPlus(int arrr[],int base)
 }
 bool cmpsame(int arr1[], int arr2[])
 {
-	if (arr1[MAX-len+1] != arr2[MAX]) return false;
+	if (arr1[MAX - len + 1] != arr2[MAX]) return false;
 	for (int i = 1; i < len; i++)
 	{
-		if (arr1[MAX-len + i+1] != arr2[MAX-len+i]) return false;
+		if (arr1[MAX - len + i + 1] != arr2[MAX - len + i]) return false;
 	}
 	return true;
 }
@@ -62,14 +67,17 @@ int main(void)
 		arr[MAX] = arrt[MAX] = rfac;
 		while (fl)
 		{
-			arrPlus(arr,bs);
-			arrPlus(arrt,bs);
+			arrPlus(arr, bs);
+			arrPlus(arrt, bs);
 			arrMulti(bs, fac);
 			if (!no && cmpsame(arr, arrt)) fl = 0;
 			else
 			{
 				memset(arr, 0, sizeof(arr));
-				memcpy(arr, arrt, sizeof(arrt));
+				for (int i = 0; i < len; i++)
+				{
+					arr[MAX - i] = arrt[MAX - i];
+				}
 			}
 			no = false;
 		}
