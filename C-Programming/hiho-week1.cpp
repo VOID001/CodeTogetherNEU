109c0dc14659cf1d77af4c25ca719bce6dc71352
VOID001 <zhangjianqiu_133@yeah.net>
2014-07-11 15:35:54 +0800

    #001 hiho-week1 TLE

--- C-Programming/hiho-week1.cpp
+++ C-Programming/hiho-week1.cpp
@@ -8,15 +8,66 @@
 #include<iostream>
 #include<stdio.h>
 #include<string.h>
+
+const int MAX=1000001;
 using namespace std;
+
+bool isPalindrome(char* head,char* end);
+
+int getMxLenPalindrome(char* str);
 
 int main(void)
 {
-	int N;
+	int N=0;
+	int res=0;
 	char str[MAX];
 	scanf("%d",&N);
 	while(N--)
 	{
-		
+		scanf("%s",str);
+		res=getMxLenPalindrome(str);
+		printf("%d\n",res);
 	}
+	return 0;
+}
+
+bool isPalindrome(char* head,char* end)
+{
+	while(head<=end)	
+	{
+		if(*head==*end) 
+		{
+			head++;
+			end--;
+		}
+		else return false;
+	}
+	return true;
+}
+
+int getMxLenPalindrome(char* str)
+{
+	int maxn=0;
+	char* pta=str;
+	char* ptb=str;
+	int len=strlen(str);
+	while(*pta!='\0')
+	{
+		ptb=pta+1;
+		while(*ptb!='\0')
+		{
+			if(*pta==*ptb) 
+			{
+				if(isPalindrome(pta,ptb)) 
+				{
+					int tmp=ptb-pta+1;
+					//printf("DEBUG: PALIN FOUND LEN=%d\n",tmp);
+					maxn=maxn>tmp?maxn:tmp;
+				}
+			}
+			ptb++;	 
+		}
+		pta++;
+	}
+	return maxn;
 }
