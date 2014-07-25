0e88a2f7534014c9e8226060437593666d57fa51
VOID001 <zhangjianqiu_133@yeah.net>
2014-07-25 20:42:42 +0800

    Problem Found

--- DataStructure/list-3.cpp
+++ DataStructure/list-3.cpp
@@ -24,9 +24,7 @@ int createListHead(CycleLinkList* Lrear,int n)
 	CycleLinkList p;
 	(*Lrear)=new Node;
 	srand(clock());
-	(*Lrear)->next=*Lrear;
-	n++;
-	while(n--)
+	(*Lrear)->next=*Lrear; n++; while(n--)
 	{
 		p=new Node;
 		p->next=(*Lrear)->next;
@@ -36,12 +34,12 @@ int createListHead(CycleLinkList* Lrear,int n)
 	return OK;
 }
 
-int merge2List(CycleLinkList* Lrear1,CycleLinkList* Lrear2)
+int merge2List(CycleLinkList Lrear1,CycleLinkList Lrear2)
 {
-	CycleLinkList p=(*Lrear1)->next;
-	(*Lrear1)->next=(*Lrear2)->next->next;
-	CycleLinkList q=(*Lrear2)->next;
-	(*Lrear2)->next=p;
+	CycleLinkList p=(Lrear1)->next;
+	(Lrear1)->next=(Lrear2)->next->next;
+	CycleLinkList q=(Lrear2)->next;
+	(Lrear2)->next=p;
 	free(q);
 	q=NULL;
 	//Lrear1=NULL;
@@ -50,13 +48,14 @@ int merge2List(CycleLinkList* Lrear1,CycleLinkList* Lrear2)
 
 void printList(CycleLinkList L)
 {
-	CycleLinkList p=L->next->next;				//游标初始定位为尾指针
+	CycleLinkList p=L->next->next;				//游标初始定位为头指针
 	while(p!=L)
 	{
 		cout<<p->data<<"->";
 		p=p->next;
 	}
 	cout<<"NULL"<<endl;
+	cout<<"DEBUG"<<L->data<<endl;
 }
 
 int main(void)
@@ -69,7 +68,10 @@ int main(void)
 	createListHead(cyclist2,2);
 	cout<<"************LIST2***************"<<endl;
 	printList(*cyclist2);
-	merge2List(cyclist1,cyclist2);
+	merge2List(*cyclist1,*cyclist2);
 	printList(*cyclist2);
 	return 0;
 }
+
+
+//这个代码会在合并之后的链表多输出一个 0 原因就是 这个循环链表建立的时候 ，尾指针没存data  默认为 0 了
