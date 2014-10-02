7096048ff706cfa62ef097f0ec045607875a5669
VOID001 <zhangjianqiu_133@yeah.net>
2014-10-02 10:23:26 +0800

    Hello

---
+++ new.cpp
@@ -0,0 +1,25 @@
+/*************************************************************************
+    > File Name: new.cpp
+    > Author: VOID_133
+    > ################### 
+    > Mail: ################### 
+    > Created Time: 2014年10月02日 星期四 10时22分01秒
+ ************************************************************************/
+#include<iostream>
+#include<algorithm>
+#include<cstdio>
+#include<vector>
+#include<cstring>
+#include<map>
+#include<queue>
+#include<stack>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+#include<set>
+using namespace std;
+
+int main(void)
+{
+	return 0;
+}
